c7524e51ee2a7e6ce27103c63c6afd8c06bef115
IKNCODE <66886403+IKNCODE@users.noreply.github.com>
2024-06-05 20:30:30 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,2 @@
+SQL_SERVER = r"DRIVER={ODBC Driver 17 for SQL Server};SERVER=195.58.51.79;Database=ltsBase;uid=test;pwd=123;Integrated Security=false"
+TOKEN_API = "6750850123:AAF69joVn5gP4i-DMENhXTS7J7VQQ_VifiA"
